89ad2e847626d65199ce0e6c12d2dbab1af8231b
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-06-16 15:41:23 +0200

    deploy attempt

--- frontend/src/pages/SinglePokemon.jsx
+++ frontend/src/pages/SinglePokemon.jsx
@@ -4,10 +4,10 @@ import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
 
 const SinglePokemon = () => {
-  
+// eslint-disable-next-line
+  const { num } = useParams()
   const [singlePokemon, setSingelPokemon] = useState([])
   useEffect(() => {
-    const { num } = useParams()
     const fetch = async () => {
       try {
         const { data } = await axios.get(
