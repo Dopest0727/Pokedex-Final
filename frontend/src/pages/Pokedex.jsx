1e1ad9e6a125ac5eb61f58db837cb745c2ee3b25
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-06-20 16:59:32 +0200

    reverted pokedex

--- frontend/src/pages/Pokedex.jsx
+++ frontend/src/pages/Pokedex.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
 import Popup from 'reactjs-popup'
 import axios from 'axios'
 import {
@@ -14,15 +13,10 @@ import {
   Badge,
   Flex,
 } from '@chakra-ui/react'
-
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-
 const Pokedex = () => {
   const [pokemons, setPokemons] = useState([])
-  const authToken = useSelector((state) => state.authenticated.authToken)
-  const userId = useSelector((state) => state.authenticated.userId)
-
   useEffect(() => {
     axios
       .get(`https://picopalquelee.herokuapp.com/pokemons`)
@@ -31,37 +25,14 @@ const Pokedex = () => {
       })
       .catch((err) => {})
   }, [])
+   const [query, setQuery] = useState('')
+   const h = useBreakpointValue({
+     base: 'repeat(1, 1fr)',
+     md: 'repeat(2, 1fr)',
+     lg: 'repeat(3, 1fr)',
+   })
 
-  const addPokemon = (event) => {
-    event.preventDefault()
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: authToken,
-      },
-      body: JSON.stringify({
-        name,
-        authenticated: userId,
-      }),
-    }
-    fetch(`https://picopalquelee.herokuapp.com/pokemons/:num`, options)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          window.location.reload()
-        }
-      })
-  }
-
-  const [query, setQuery] = useState('')
-  const h = useBreakpointValue({
-    base: 'repeat(1, 1fr)',
-    md: 'repeat(3, 1fr)',
-    lg: 'repeat(5, 1fr)',
-  })
-
-  const backgrounds = {
+   const backgrounds = {
     Normal: '#A8A878', // a8a878 //BLACKALPHA CHECK
     Ground: '#E0C068', // e0c068
     Fighting: '#C02038', // c02038
@@ -78,7 +49,6 @@ const Pokedex = () => {
     Ice: '#98D8D8', // #98D8D8 //YELLOW CHECK
     Electric: '#F8D030', // #F8D030
   }
-
   return (
     <Box>
       <NavBar />
@@ -101,169 +71,185 @@ const Pokedex = () => {
                   pokemon.name.toLowerCase().includes(query.toLowerCase())
                 ) {
                   return pokemon
-                }
-              })
-              .map((pokemon) => {
-                console.log(pokemons)
-                return (
-                  <Box
-                    w="100%"
-                    key={pokemon._id}
-                    bgColor={backgrounds[pokemon.type[0]]}
-                  >
-                    <Box>
-                      <Stack
+                 }
+               })
+               .map((pokemon) => {
+                 return (
+                   <Box
+                     w="xs"
+                     borderWidth="2px"
+                     borderColor="blue.200"
+                     borderRadius="lg"
+                     key={pokemon._id}
+                   >
+                     <Box>
+                       <Stack
                         w="100%"
                         display="flex"
-                        direction="column"
-                        alignItems="center"
-                      >
-                        <Box
-                          key={pokemon.type}
-                          borderRadius="md"
-                          textAlign="center"
-                        >
-                          <p>#{pokemon.num}</p>
-                          <Center>
-                            <Image m="5" src={pokemon.img} alt={pokemon.name} />
-                          </Center>
-                          <b>{pokemon.name}</b>
-                          <br />
-                          Type: {pokemon.type[0]} {pokemon.type[1]}
-                        </Box>
-
-                        <Flex w="100%" display="flex" direction="column">
-                          <Box>
-                            <Popup
-                              trigger={
-                                <Button
-                                  bgColor={backgrounds[pokemon.type[0]]}
-                                  w="100%"
-                                  borderRadius="0"
-                                  color="black"
-                                  variant="solid"
-                                  _hover={{
-                                    background: 'blue.500',
-                                    color: 'white',
-                                  }}
-                                >
-                                  Read more
-                                </Button>
-                              }
-                              modal
-                              nested
-                            >
-                              {(close) => (
-                                <Box
-                                  top={'0'}
-                                  left={'0'}
-                                  width={'100vw'}
-                                  height={'100vh'}
-                                  blur={'3px'}
-                                  background={'rgba(0, 0, 0, 0.7)'}
-                                  display={'flex'}
-                                  alignItems={'center'}
-                                  justifyContent={'center'}
-                                >
-                                  <Box
-                                    w="xs"
-                                    bgColor={backgrounds[pokemon.type[0]]}
-                                    p="10"
-                                    justifyContent="center"
-                                  >
-                                    <Center>
-                                      <Image
-                                        width="50%"
-                                        src={pokemon.img}
-                                        alt={pokemon.name}
-                                        mb="10"
-                                      />
-                                    </Center>
-                                    <Stack
-                                      direction="column"
-                                      justify="space-between"
-                                    >
-                                      <Box
-                                        display={'flex'}
-                                        alignItems={'center'}
-                                        justifyContent={'center'}
-                                        flexDir={'column'}
-                                      >
-                                        <h1>Number: {pokemon.num}</h1>
-                                        <h1>Name: {pokemon.name} </h1>
-                                        <h1> Type: {pokemon.type[0]} </h1>
-                                        <h1> {pokemon.type[1]} </h1>
-                                        <h1> Height: {pokemon.height} </h1>
-                                        <h1>Weight: {pokemon.weight} </h1>
-                                        <h1>
-                                          Weaknesses: {pokemon.weaknesses[0]},
-                                          {pokemon.weaknesses[1]}
-                                        </h1>
-                                        
-                                        Evolutions:
-                                        {pokemon.next_evolution.map((y) => (
-                                          <div key={pokemon.id}>
-                                            {y.name}
-                                          </div>
-                                        ))}
-                                      </Box>
-                                    </Stack>
-                                    <Button
-                                      mt="4"
-                                      w="100%"
-                                      onClick={close}
-                                      bgColor="blue.200"
-                                      color="white"
-                                    >
-                                      Height: {pokemon.height}
-                                    </Badge>
-                                    <Badge
-                                      color="white"
-                                      bgColor="blue.400"
-                                      variant="solid"
-                                      borderRadius="md"
-                                      textAlign="left"
-                                      p="2"
-                                    >
-                                      Weight: {pokemon.weight}
-                                    </Badge>
-                                    <Badge
-                                      color="white"
-                                      bgColor="blue.300"
-                                      variant="solid"
-                                      borderRadius="md"
-                                      textAlign="left"
-                                      p="2"
-                                    >
-                                      Weaknesses: {pokemon.weaknesses[0]}{' '}
-                                      {pokemon.weaknesses[1]}
-                                    </Badge>
-                                  </Stack>
-                                  <Button
-                                    mt="4"
-                                    w="100%"
-                                    onClick={close}
-                                    bgColor="blue.200"
-                                    color="white"
-                                  >
-                                    {' '}
-                                    Close{' '}
-                                  </Button>
-                                </Box>
-                              )}
-                            </Popup>
-                          </Box>
-                        </Flex>
-                        <Button
-                          onChange={() => {
-                            addPokemon(userId, pokemon.name)
-                          }}
-                        >
-                          Add
-                        </Button>
-                      </Stack>
-                    </Box>
-                  </Box>
+                         direction="column"
+                         alignItems="center"
+                       >
+                         <Box bgColor="blue.100" w="100%" borderRadius="md">
+                           <Center>
+                             <Image
+                               m="5"
+                               src={pokemon.img}
+                               alt={pokemon.name}
+                               borderRadius="lg"
+                             />
+                           </Center>
+                         </Box>
+                         <Flex px="2" w="100%" display="flex" direction="column">
+                           <Badge
+                             bgColor="blue.300"
+                             mb="2"
+                             w="100%"
+                             color="white"
+                             variant="solid"
+                             borderRadius="md"
+                             textAlign="center"
+                             py="3"
+                             px="3"
+                           >
+                             {pokemon.name}
+                           </Badge>
+                           <Popup
+                             trigger={
+                               <Button
+                                 bgColor="blue.300"
+                                 w="100%"
+                                 color="white"
+                                 variant="solid"
+                                 _hover={{
+                                   background: 'blue.500',
+                                   color: 'white',
+                                 }}
+                               >
+                                 Read more
+                               </Button>
+                             }
+                             modal
+                             nested
+                           >
+                             {(close) => (
+                               <Box>
+                                 <Box
+                                   w="xs"
+                                   bgColor="white"
+                                   borderRadius="lg"
+                                   borderWidth="2px"
+                                   borderColor="blue.200"
+                                   p="10"
+                                   justifyContent="center"
+                                 >
+                                   <Center>
+                                     <Image
+                                       width="50%"
+                                       src={pokemon.img}
+                                       alt={pokemon.name}
+                                       mb="10"
+                                     />
+                                   </Center>
+                                   <Stack
+                                     direction="column"
+                                     justify="space-between"
+                                   >
+                                     <Badge
+                                       color="white"
+                                       bgColor="blue.800"
+                                       variant="solid"
+                                       borderRadius="md"
+                                       textAlign="left"
+                                       p="2"
+                                     >
+                                       Number: {pokemon.num}
+                                     </Badge>
+                                     <Badge
+                                       color="white"
+                                       bgColor="blue.700"
+                                       variant="solid"
+                                       borderRadius="md"
+                                       textAlign="left"
+                                       p="2"
+                                     >
+                                       Name: {pokemon.name}
+                                     </Badge>
+                                     <Badge
+                                       color="white"
+                                       bgColor="blue.600"
+                                       variant="solid"
+                                       borderRadius="md"
+                                       textAlign="left"
+                                       p="2"
+                                     >
+                                       Type: {pokemon.type[0]} {pokemon.type[1]}
+                                     </Badge>
+                                     <Badge
+                                       color="white"
+                                       bgColor="blue.500"
+                                       variant="solid"
+                                       borderRadius="md"
+                                       textAlign="left"
+                                       p="2"
+                                     >
+                                       Height: {pokemon.height}
+                                     </Badge>
+                                     <Badge
+                                       color="white"
+                                       bgColor="blue.400"
+                                       variant="solid"
+                                       borderRadius="md"
+                                       textAlign="left"
+                                       p="2"
+                                     >
+                                       Weight: {pokemon.weight}
+                                     </Badge>
+                                     <Badge
+                                       color="white"
+                                       bgColor="blue.300"
+                                       variant="solid"
+                                       borderRadius="md"
+                                       textAlign="left"
+                                       p="2"
+                                     >
+                                       Weaknesses: {pokemon.weaknesses[0]} {pokemon.weaknesses[1]}
+                                     </Badge>
+                                   </Stack>
+                                   <Button
+                                     mt="4"
+                                     w="100%"
+                                     onClick={close}
+                                     bgColor="blue.200"
+                                     color="white"
+                                   >
+                                     {' '}
+                                     Close{' '}
+                                   </Button>
+                                 </Box>
+                               </Box>
+                             )}
+                           </Popup>
+                         </Flex>
+                         <Flex w="100%" display="flex" direction="row" px="2">
+                           {pokemon.type.map((type) => (
+                             <Badge
+                               borderRadius="md"
+                               w="100%"
+                               textAlign="center"
+                               color="white"
+                               py="3"
+                               px="3"
+                               mb="2"
+                               bgColor={backgrounds[type]}
+                               key={type}
+                             >
+                               {type}
+                             </Badge>
+                           ))}
+                         </Flex>
+                       </Stack>
+                     </Box>
+                   </Box>
                 )
               })}
           </Grid>
@@ -273,5 +259,4 @@ const Pokedex = () => {
     </Box>
   )
 }
-
 export default Pokedex
