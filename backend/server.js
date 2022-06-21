b01429b3c6bc317834d26c0c40f80cac59f571aa
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-06-21 16:09:08 +0200

    userauth

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,25 @@ const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/pokedex-api'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// const authenticateUser = async (req, res, next) => {
-//   const accessToken = req.header("Authorization");
-//   try {
-//     const user = await User.findOne({ accessToken: accessToken });
-//     if (user) {
-//       next();
-//     } else {
-//       res.status(401).json({
-//         response: "Please log in",
-//         success: false,
-//       });
-//     }
-//   } catch (error) {
-//     res.status(400).json({
-//       response: error,
-//       success: false,
-//     });
-//   }
-// };
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken: accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: "Please log in",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+    });
+  }
+};
 
 const port = process.env.PORT || 8080
 const app = express()
